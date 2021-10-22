41dbae5c66ce9a991f0b7d0662248fddd788f2c8
Ben Sampica <benjamin.sampica@gmail.com>
2021-10-21 20:51:49 -0500

    Initial release.

---
+++ README.md
@@ -0,0 +1,14 @@
+# About
+
+[![.NET](https://github.com/benjaminsampica/SamsonsForge.Extensions/actions/workflows/dotnet.yml/badge.svg)](https://github.com/benjaminsampica/SamsonsForge.Extensions/actions/workflows/dotnet.yml)
+
+This repository and package is intended to provide extensions for common patterns when working with modern .NET applications.
+
+
+# Using
+
+Currently, there is only one single service collection extension for when registering configuration options but more will be added over time. 
+
+## SamsonsForge.Extensions.Configuration
+
+Add `using SamsonsForge.Extensions.Configuration` to the top of your C# file.
---
+++ SamsonsForge.Extensions.Configuration/ConfigurationExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace SamsonsForge.Extensions.Configuration
+{
+    /// <summary>
+    ///     Common extensions for <see cref="IServiceCollection"/> when handling patterns for <see cref="IConfiguration"/>.
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        ///     <para>
+        ///         Registers a configuration instance which TOptions will bind against. 
+        ///         Additionally, registers the <see cref="IOptions{TOptions}.Value"/> as a scoped service to the service collection.
+        ///     </para>
+        ///     
+        ///     Allows dependency injection of the given options directly rather than through an <see cref="IOptions{TOptions}"/>.
+        /// </summary>
+        /// <example>
+        /// </example>
+        /// <returns>A <see cref="IServiceCollection"/> so calls that additional calls be chained.</returns>
+        public static IServiceCollection ConfigureAsScoped<TOptions>(this IServiceCollection services, IConfiguration configuration, string key)
+            where TOptions : class
+        {
+            return services.Configure<TOptions>(configuration.GetSection(key))
+                .AddScoped(s => s.GetRequiredService<IOptions<TOptions>>().Value);
+        }
+    }
+}
